a997214cbeb5c3aa7c15b6489b5dc35d065a319a
Sebastian Kralicek <kraliseb@gmail.com>
2023-12-02 02:41:34 +0100

    [1*] finished first half

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"log"
+	"strconv"
+	"os"
 )
 
 func main() {
-    fmt.Print("Hello World")
+    // Objective: Read input, for each line take the first and last digit and concat them
+    // Sum the result of all lines.
+    f, err := os.Open("input")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+    sum := 0
+    for scanner.Scan() {
+	sum += sumFirstAndLast(scanner.Text())
+    }
+    fmt.Println(sum)
+}
+
+func sumFirstAndLast(input string) int {
+    first := -1
+    last := -1
+    for _, v := range input {
+	number, err := strconv.Atoi(string(v))
+	if err != nil {
+	    continue
+	} else if first == -1 {
+	    first = number
+	    last = number
+	} else {
+	    last = number
+	}
+    }
+
+    return 10 * first + last
 }
